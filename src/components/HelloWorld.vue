135298cc46c4fb4ed7301d432aca2a3ea1e1809a
asmfadholi <asmfadholi@gmail.com>
2018-08-07 21:51:44 +0700

    vue access event in other component

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>  
+  <div>
+      <vue-clockpicker :value="searchText" @timeset="searchText = $event" />
+    <h1>{{ searchText }}</h1>
+  </div>
+</template>
+
+<script>
+import VueClockPicker from '@pencilpix/vue2-clock-picker';
+import '@pencilpix/vue2-clock-picker/dist/vue2-clock-picker.min.css';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    'vue-clockpicker': VueClockPicker,
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      searchText: '20:00',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
